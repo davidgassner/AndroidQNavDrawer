a1447961055d1103188ac92010b8ffb416e32f30
David Gassner <david.gassner@gmail.com>
2019-05-21 23:50:23 -0700

    Use custom DrawerLayout with border exclusion

--- app/src/main/java/com/example/android/qnavdrawer/ui/CustomDrawerLayout.kt
+++ app/src/main/java/com/example/android/qnavdrawer/ui/CustomDrawerLayout.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
-import android.util.Log
+import androidx.core.os.BuildCompat
 import androidx.drawerlayout.widget.DrawerLayout
 
 class CustomDrawerLayout : DrawerLayout {
@@ -15,11 +15,13 @@ class CustomDrawerLayout : DrawerLayout {
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         super.onLayout(changed, l, t, r, b)
 
-        Log.i("AndroidQ", "onLayout: $l, $t, $r, $b")
-
-        @SuppressLint("DrawAllocation")
-        val rectangle = Rect(0, 0, 100, b)
-        systemGestureExclusionRects = listOf(rectangle)
+        // TODO: replace condition with version check after API has been finalized
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        if (BuildCompat.isAtLeastQ()) {
+            @SuppressLint("DrawAllocation")
+            val rectangle = Rect(0, 0, 100, b)
+            systemGestureExclusionRects = listOf(rectangle)
+        }
 
     }
 
